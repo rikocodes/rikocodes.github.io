2f0ea0e904b7e892e6c0da7633a9619b62f35061
rikocodes <rikocodes@github.com>
2019-09-08 16:06:23 +0100

    Updates

---
+++ precache-manifest.7c4ba0d244dc382bd4cbdeca440cf945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774646b207d3b2caad2e2cd70aa07ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4aa0e595303f1ca52d6",
+    "url": "/static/css/main.eb99166d.chunk.css"
+  },
+  {
+    "revision": "4e6dd79208d679714e3b",
+    "url": "/static/js/2.cf4c255a.chunk.js"
+  },
+  {
+    "revision": "a4aa0e595303f1ca52d6",
+    "url": "/static/js/main.2d7e71e3.chunk.js"
+  },
+  {
+    "revision": "47082a01e5e535349806",
+    "url": "/static/js/runtime~main.8a3ca1da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.3e37bdf89a3d4caa0abd2821439f1d81.js"
+  "/precache-manifest.7c4ba0d244dc382bd4cbdeca440cf945.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
