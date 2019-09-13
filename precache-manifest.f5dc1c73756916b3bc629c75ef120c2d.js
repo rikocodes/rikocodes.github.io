11a47f6b09bd67af20788ee1771bc757d9da4b68
rikocodes <rikocodes@github.com>
2019-09-13 18:31:39 +0100

    Updates

---
+++ precache-manifest.f5dc1c73756916b3bc629c75ef120c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f876e3fca8e53ede7ef126091db237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc099b04c88cb9939a07",
+    "url": "/static/css/main.eb99166d.chunk.css"
+  },
+  {
+    "revision": "c8a7fefbe00622628878",
+    "url": "/static/js/2.8bbc7425.chunk.js"
+  },
+  {
+    "revision": "cc099b04c88cb9939a07",
+    "url": "/static/js/main.0e10a402.chunk.js"
+  },
+  {
+    "revision": "f715bfc6832eee5f22b4",
+    "url": "/static/js/runtime~main.8c172361.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c4ba0d244dc382bd4cbdeca440cf945.js"
+  "/precache-manifest.f5dc1c73756916b3bc629c75ef120c2d.js"
 );
 
 self.addEventListener('message', (event) => {
